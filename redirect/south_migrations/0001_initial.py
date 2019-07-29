bec97f43d35a818fed1c0f08573d7fe24e44a6bd
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-29 10:11:14 +0400

    Remove old style strings

--- redirect/south_migrations/0001_initial.py
+++ redirect/south_migrations/0001_initial.py
@@ -9,8 +9,8 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Redirect'
-        db.create_table(u'redirect_redirect', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('redirect_redirect', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
             ('from_url', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255, db_index=True)),
             ('to_url', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
@@ -20,22 +20,22 @@ class Migration(SchemaMigration):
             ('create_dt', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('update_dt', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
-        db.send_create_signal(u'redirect', ['Redirect'])
+        db.send_create_signal('redirect', ['Redirect'])
 
         # Adding unique constraint on 'Redirect', fields ['site', 'from_url']
-        db.create_unique(u'redirect_redirect', ['site_id', 'from_url'])
+        db.create_unique('redirect_redirect', ['site_id', 'from_url'])
 
 
     def backwards(self, orm):
         # Removing unique constraint on 'Redirect', fields ['site', 'from_url']
-        db.delete_unique(u'redirect_redirect', ['site_id', 'from_url'])
+        db.delete_unique('redirect_redirect', ['site_id', 'from_url'])
 
         # Deleting model 'Redirect'
-        db.delete_table(u'redirect_redirect')
+        db.delete_table('redirect_redirect')
 
 
     models = {
-        u'actstream.action': {
+        'actstream.action': {
             'Meta': {'ordering': "('-timestamp',)", 'object_name': 'Action'},
             'action_object_content_type': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'action_object'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
             'action_object_object_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
@@ -43,7 +43,7 @@ class Migration(SchemaMigration):
             'actor_object_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'batch_time_minutes': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_batchable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'state': ('django.db.models.fields.SmallIntegerField', [], {'default': '1'}),
@@ -53,29 +53,29 @@ class Migration(SchemaMigration):
             'timestamp_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime.now'}),
             'verb': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
-        u'contenttypes.contenttype': {
+        'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        u'redirect.redirect': {
+        'redirect.redirect': {
             'Meta': {'ordering': "('-uses_regex',)", 'unique_together': "(('site', 'from_url'),)", 'object_name': 'Redirect'},
             'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'from_url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
             'http_status': ('django.db.models.fields.SmallIntegerField', [], {'default': '301'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
             'status': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'to_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
             'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'uses_regex': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         },
-        u'sites.site': {
+        'sites.site': {
             'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
             'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }

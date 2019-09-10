d41b2cb6a06ad7490a37f4411b34676d32b52630
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-09-10 12:04:35 +0400

    Undo migration in django.contrib.auth

--- redirect/dynamic_urls.py
+++ redirect/dynamic_urls.py
@@ -1,4 +1,4 @@
 from .utils import get_redirect_patterns
 # Comment out the following line of code when testing, or migrating.
 # Otherwise a database error will occur.
-urlpatterns = get_redirect_patterns()
+#urlpatterns = get_redirect_patterns()
